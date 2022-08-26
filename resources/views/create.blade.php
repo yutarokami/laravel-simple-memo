f08bf56973a45cf312a74b498674358584612ae6
yutarokami <kamitch01@gmail.com>
2022-08-26 16:02:52 +0900

    新規メモ作成

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,23 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="card">
+    <div class="card-header">新規メモ作成</div>
+    <form class="card-body" action="/store" method="POST">
+        @csrf
+        <div class="form-group">
+            <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
+            <button type="submit" class="btn btn-primary">保存</button>
         </div>
-    </div>
+</form>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,6 @@
                     </div>
                 </div>
                 <div class="col-md-6 p-0">
-                    右カラム
                     @yield('content')
                 </div>
             </div>
