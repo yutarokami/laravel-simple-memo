5a4ebae1dca553ce99dda3fdec586df800732beb
yutarokami <kamitch01@gmail.com>
2022-08-26 12:53:25 +0900

    テーブルの作成

--- database/migrations/2022_08_25_235010_create_memos_table.php
+++ database/migrations/2022_08_25_235010_create_memos_table.php
@@ -15,13 +15,13 @@ class CreateMemosTable extends Migration
     {
         Schema::create('memos', function (Blueprint $table) {
             $table->unsignedBigInteger('id',true);
-            $table->longtext('content');
+            $table->longText('content');
             $table->unsignedBigInteger('user_id');
             $table->softDeletes();
 
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2022_08_25_235622_create_tags_table.php
+++ database/migrations/2022_08_25_235622_create_tags_table.php
@@ -14,8 +14,14 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id',true);
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
