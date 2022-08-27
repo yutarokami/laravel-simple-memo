91e71fcc9043578951a80163cd4c05b89dff2efe
yutarokami <kamitch01@gmail.com>
2022-08-27 14:50:26 +0900

    タグによる絞り込み機能を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,16 +27,30 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*', function ($view) {
-            $memos = Memo::select('memos.*')
-            ->where('user_id','=',\Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at','DESC')
-            ->get();
+            $query_tag = \Request::query('tag');
+            // もしクエリパラメータがあれば
+            if ( !empty( $query_tag ) ) {
+                // タグで絞込
+                $memos = Memo::select('memos.*')
+                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                    ->where('memo_tags.tag_id','=', $query_tag)
+                    ->where('user_id','=',\Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at','DESC')
+                    ->get();
+            } else {
+                // タグがなければすべて取得
+                $memos = Memo::select('memos.*')
+                    ->where('user_id','=',\Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at','DESC')
+                    ->get();
+            }
 
             $tags = Tag::where('user_id','=',\Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('id','DESC')
-            ->get();
+                ->whereNull('deleted_at')
+                ->orderBy('id','DESC')
+                ->get();
 
             $view->with('memos', $memos)->with('tags', $tags);
         });
